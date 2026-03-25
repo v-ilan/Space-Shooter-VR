fe9c3c9c1aa479d698ddad0a86a367f25bb4c761
v-ilan <ilanvol96@gmail.com>
2026-03-25 19:36:39 +0200

    locomotion setup UI

--- Space Scrapper/Assets/Scripts/UI/LocomotionSetupUI.cs
+++ Space Scrapper/Assets/Scripts/UI/LocomotionSetupUI.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class LocomotionSettingsUI : MonoBehaviour
+public class LocomotionSetupUI : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
