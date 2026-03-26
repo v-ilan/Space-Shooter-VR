0719b83418410bcc18db6d75152df41220819862
v-ilan <ilanvol96@gmail.com>
2026-03-26 15:03:47 +0200

    Update NarrativeStepSO.cs

--- Space Scrapper/Assets/Scripts/ScriptableObjects/NarrativeStepSO.cs
+++ Space Scrapper/Assets/Scripts/ScriptableObjects/NarrativeStepSO.cs
@@ -1,7 +1,19 @@
+using System;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "NarrativeStepSO", menuName = "Scriptable Objects/NarrativeStepSO")]
 public class NarrativeStepSO : ScriptableObject
 {
-    
+    public string stepName;
+    public double startTime; // Where in the Timeline this beat starts
+    public bool isCompleted;
+
+    public event EventHandler OnStepActivated;
+
+    public void Activate()
+    {
+        if (isCompleted) return;
+        isCompleted = true;
+        OnStepActivated?.Invoke(this, EventArgs.Empty);
+    }
 }
