bb113d503caeed694fb7c12c8d95b1be0ec3023a
v-ilan <ilanvol96@gmail.com>
2026-03-26 16:37:55 +0200

    Update TrashCan.cs

--- Space Scrapper/Assets/Scripts/TrashCan.cs
+++ Space Scrapper/Assets/Scripts/TrashCan.cs
@@ -2,25 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 
 public class TrashCan : MonoBehaviour
 {
-    //public event EventHandler OnObjectTrashed;
-    private NarrativeTrigger narrativeTrigger;
-
-    private void Start()
-    {
-        narrativeTrigger = GetComponent<NarrativeTrigger>();
-    }
+    public UnityEvent OnObjectTrashed;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.GetComponent<XRGrabInteractable>() != null)
         {
             other.gameObject.SetActive(false);
-            narrativeTrigger.TriggerStep();
+            OnObjectTrashed?.Invoke();
         }
     }
 }
