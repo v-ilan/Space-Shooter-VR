d4174dd6d04103d5dd8ee8e43dbefb02cad2c464
v-ilan <ilanvol96@gmail.com>
2026-03-27 16:13:43 +0300

    Update NarativeOver.cs

--- Space Scrapper/Assets/Scripts/NarativeOver.cs
+++ Space Scrapper/Assets/Scripts/NarativeOver.cs
@@ -2,15 +2,35 @@ using UnityEngine;
 
 public class NarativeOver : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Transition Settings")]
+    [SerializeField] private SceneLoader.Scene targetScene = SceneLoader.Scene.StartMenuScene;
+    
+    [Header("Debug")]
+    [SerializeField] private bool logTransition = true;
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Triggered by the SIG_END Signal Emitter in the Timeline.
+    /// </summary>
+    public void OnNarrativeSequenceComplete()
     {
-        
+        if (logTransition)
+        {
+            Debug.Log($"[NarrativeOver]Signal Received. Transitioning to {targetScene}...");
+        }
+
+        // Check if the Fader exists before calling it to prevent NullRefs during testing
+        if (SceneTransitionUI.Instance != null)
+        {
+            SceneTransitionUI.Instance.FadeOut(() => 
+            {
+                SceneLoader.Load(targetScene);
+            });
+        }
+        else
+        {
+            // Safety: If for some reason the fader is missing, load the scene anyway
+            Debug.LogWarning("[NarrativeOver] SceneTransitionUI Instance missing! Loading scene immediately.");
+            SceneLoader.Load(targetScene);
+        }
     }
 }
