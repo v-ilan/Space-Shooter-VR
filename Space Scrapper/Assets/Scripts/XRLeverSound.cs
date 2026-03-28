5f3ecad007433d2d1b284d91ec71916bfb664f0f
v-ilan <ilanvol96@gmail.com>
2026-03-28 20:06:13 +0300

    Update XRLeverSound.cs

--- Space Scrapper/Assets/Scripts/XRLeverSound.cs
+++ Space Scrapper/Assets/Scripts/XRLeverSound.cs
@@ -1,16 +1,49 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.XR.Content.Interaction;
 
+[RequireComponent(typeof(AudioSource))]
 public class XRLeverSound : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private XRLever xrLever;
+    private AudioSource audioSource;
+
+    private void Awake()
     {
+        if (xrLever == null) xrLever = GetComponent<XRLever>();
+        audioSource = GetComponent<AudioSource>();
+
+        // Engine Polish
+        audioSource.loop = true;
+        audioSource.playOnAwake = false;
         
+        // Set to 0.5f for "Spatial-Stereo" - feels like it's all around the ship
+        audioSource.spatialBlend = 0.5f; 
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable()
     {
-        
+        xrLever.onLeverActivate.AddListener(OnEngineStart);
+        xrLever.onLeverDeactivate.AddListener(OnEngineStop);
+
+        SoundManager.Instance.OnVolumeChange += SoundManager_OnVolumeChange;
+        UpdateVolume();
     }
+
+    void OnDisable()
+    {
+        xrLever.onLeverActivate.RemoveListener(OnEngineStart);
+        xrLever.onLeverDeactivate.RemoveListener(OnEngineStop);
+        
+        SoundManager.Instance.OnVolumeChange -= SoundManager_OnVolumeChange;
+    }
+
+    private void SoundManager_OnVolumeChange(object sender, EventArgs e) => UpdateVolume();
+
+    private void UpdateVolume() => audioSource.volume = SoundManager.Instance.GetVolume();
+
+    private void OnEngineStart() => audioSource.Play();
+
+    private void OnEngineStop() => audioSource.Stop();
 }
